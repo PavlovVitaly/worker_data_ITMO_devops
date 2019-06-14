e98b994ac854ce4fd9a6788d750c3494aa10a414
Pavlov Vitaly <pvs.mazilla@gmail.com>
2019-06-14 20:53:59 +0300

    clear worker

--- worker/myapp/middleware/ResultOfTest.py
+++
@@ -1,7 +0,0 @@
-class ResultOfTest:
-    name=None
-    num_of_right_answers = None
-
-    def __init__(self, name, num_of_right_answers):
-        self.name = name
-        self.num_of_right_answers = num_of_right_answers
